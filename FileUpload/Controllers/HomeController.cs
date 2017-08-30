d1a28b144a7613224ece09c7cd8097dede5d5db0
Jennifer Bartolome <jbartolome@users.noreply.github.com>
2017-08-30 08:06:54 -0700

    Added initial files via upload

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FileUpload.Models;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Upload(FormCollection formCollection)
+        {
+            if (Request != null)
+            {
+                HttpPostedFileBase file = Request.Files["UploadedFile"];
+                if ((file != null) && (file.ContentLength > 0) && !string.IsNullOrEmpty(file.FileName))
+                {
+                    string fileName = file.FileName;
+                    string fileContentType = file.ContentType;
+                    byte[] fileBytes = new byte[file.ContentLength];
+                    var data = file.InputStream.Read(fileBytes, 0, Convert.ToInt32(file.ContentLength));
+                    var usersList = new List<Users>();
+                    using (var package = new ExcelPackage(file.InputStream))
+                    {
+                        var currentSheet = package.Workbook.Worksheets;
+                        var workSheet = currentSheet.First();
+                        var noOfCol = workSheet.Dimension.End.Column;
+                        var noOfRow = workSheet.Dimension.End.Row;
+
+                        for (int rowIterator = 2; rowIterator <= noOfRow; rowIterator++)
+                        {
+                            var user = new Users();
+                            user.EmployeeId = workSheet.Cells[rowIterator, 1].Value.ToString();
+                            user.FullName = workSheet.Cells[rowIterator, 2].Value.ToString();
+                            user.BenefitCode = workSheet.Cells[rowIterator, 3].Value.ToString();
+                            usersList.Add(user);
+                        }
+                    }
+                    return View("Index",usersList);
+                }
+
+                
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ FileUpload/Models/Users.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class Users
+    {
+        private string fullName;
+        private string employeeId;
+        private string benefitCode;
+
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+
+            set
+            {
+                fullName = value;
+            }
+        }
+
+        public string EmployeeId
+        {
+            get
+            {
+                return employeeId;
+            }
+
+            set
+            {
+                employeeId = value;
+            }
+        }
+
+        public string BenefitCode
+        {
+            get
+            {
+                return benefitCode;
+            }
+
+            set
+            {
+                benefitCode = value;
+            }
+        }
+    }
+}
